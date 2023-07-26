52deee146eca4017f2a12eb8609011403312cbb2
yugasun <yuga_sun@163.com>
2023-07-26 13:34:14 +0800

    fix: update post user api

--- src/main/java/com/example/swapp/controller/UserController.java
+++ src/main/java/com/example/swapp/controller/UserController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,16 +31,18 @@ public class UserController {
         return "info";
     }
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     public List<User> usersList() throws InterruptedException {
         Thread.sleep(randomSleepLoop(sleepMin, sleepMax));
         return userRepository.findAll();
     }
 
-    @PostMapping("/users")
-    public User users() throws InterruptedException {
+    @PostMapping("/user")
+    public User users(
+            @RequestBody String name
+    ) throws InterruptedException {
         Thread.sleep(randomSleepLoop(sleepMin, sleepMax));
-        return userRepository.save(User.builder().name("user").build());
+        return userRepository.save(User.builder().name(name).build());
     }
 
     @PostMapping("/correlation")
